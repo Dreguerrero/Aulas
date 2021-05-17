3cb0b74bc032cd5933b1748d9099d288c2a290e8
Dreguerrero <andressa.guerrero@gmail.com>
2021-05-17 16:15:08 -0300

    Create exercicio2.py

---
+++ exercicio2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list_1 = [1,2,3]
+soma_list_1 = 0
+for x in list_1:
+    soma_list_1 += x
+
+print(soma_list_1)
+
+list_2 = [4,5,6]
+soma_list_2 = 0
+for y in list_2:
+    soma_list_2 += y
+
+print(soma_list_2)
+
+
+print (soma_list_2*soma_list_1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
